d19a3b145c9684aa4b42097c8c64817487f3f1cb
cyeogy <jestaqw167@gmail.com>
2025-05-19 06:34:54 +0900

    add controller

---
+++ src/main/java/com/fastcampus/sns/UserController.java
@@ -0,0 +1,13 @@
+package com.fastcampus.sns;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    @GetMapping("/")
+    public String helloWorld() {
+        return "Hello World!";
+    }
+}
