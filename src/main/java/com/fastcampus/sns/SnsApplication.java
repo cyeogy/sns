ad279623f3f98bf14393e5314786aa9f3312ef67
cyeogy <jestaqw167@gmail.com>
2025-06-20 22:24:43 +0900

    delete exclude configuration

--- src/main/java/com/fastcampus/sns/SnsApplication.java
+++ src/main/java/com/fastcampus/sns/SnsApplication.java
@@ -8,7 +8,7 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 
-@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
+@SpringBootApplication
 public class SnsApplication {
 
 	public static void main(String[] args) {
