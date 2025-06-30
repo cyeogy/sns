38a476821c5a867a67b1badb1fb64f230def8fc3
cyeogy <jestaqw167@gmail.com>
2025-07-01 00:52:46 +0900

    development for post service

--- src/main/java/com/fastcampus/sns/configuration/AuthenticationConfig.java
+++ src/main/java/com/fastcampus/sns/configuration/AuthenticationConfig.java
@@ -1,17 +1,28 @@
 package com.fastcampus.sns.configuration;
 
+import com.fastcampus.sns.configuration.filter.JwtTokenFilter;
+import com.fastcampus.sns.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class AuthenticationConfig extends WebSecurityConfigurerAdapter {
+
+    private final UserService userService;
+    @Value("${jwt.secret-key}")
+    private String key;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+
         http.csrf().disable()
                 .authorizeRequests()
                 .antMatchers("/api/*/users/join", "/api/*/users/login").permitAll()
@@ -19,6 +30,8 @@ public class AuthenticationConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .addFilterBefore(new JwtTokenFilter(key, userService), UsernamePasswordAuthenticationFilter.class)
         ;
 
         // TODO
---
+++ src/main/java/com/fastcampus/sns/model/entity/PostEntity.java
@@ -0,0 +1,63 @@
+package com.fastcampus.sns.model.entity;
+
+import com.fastcampus.sns.model.UserRole;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+import java.time.Instant;
+
+@Entity
+@Table(name = "\"post\"")
+@Setter
+@Getter
+@SQLDelete(sql = "UPDATED \"post\" SET deleted_at = NOW() where id=?")
+@Where(clause = "deleted_at is NULL")
+public class PostEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "body", columnDefinition = "TEXT")
+    private String body;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private UserEntity user;
+
+    // 나중에 에러가 발생했을 때 디버깅하기 쉽게 하기 위해 추가
+    @Column(name = "registered_at")
+    private Timestamp registeredAt;
+
+    @Column(name = "updated_at")
+    private Timestamp updatedAt;
+
+    @Column(name = "deleted_at")
+    private Timestamp deletedAt;
+
+    @PrePersist
+    void registeredAt() {
+        this.registeredAt = Timestamp.from(Instant.now());
+    }
+
+    @PreUpdate
+    void updatedAt() {
+        this.updatedAt = Timestamp.from(Instant.now());
+    }
+
+    public static PostEntity of(String title, String body, UserEntity userEntity) {
+        PostEntity entity = new PostEntity();
+        entity.setTitle(title);
+        entity.setBody(body);
+        entity.setUser(userEntity);
+        return entity;
+    }
+}
+
