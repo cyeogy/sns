3c8ec27c75802465a7433eb2b5d64ac29e4a1464
cyeogy <jestaqw167@gmail.com>
2025-08-04 00:07:18 +0900

    add test controller method

--- src/main/java/com/fastcampus/sns/controller/TestController.java
+++ src/main/java/com/fastcampus/sns/controller/TestController.java
@@ -10,4 +10,9 @@ public class TestController {
     public String test() {
         return "test";
     }
+
+    @GetMapping("/test2")
+    public String test2() {
+        return "test2";
+    }
 }
